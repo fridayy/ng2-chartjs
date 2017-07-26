5e1e19400a11660265b188571dcfcd3fac2064b6
fridayy <bnjm@harmless.ninja>
2017-07-27 00:15:42 +0200

    asdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'chart js demo';
+  // define the type (line, bar etc.)
+  type = 'line';
+  // data
+  data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
+  // options of the chart
+  options = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  constructor() {}
 }
